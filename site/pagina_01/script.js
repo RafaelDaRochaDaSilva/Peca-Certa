32cb8e1c3f5e012d9dba66fc7e7b5b29b276067b
juliocesar1404 <julio06.rosa@catolicasc.edu.br>
2025-05-01 23:50:50 -0300

    Add files via upload

---
+++ site/pagina_01/script.js
@@ -0,0 +1,11 @@
+function toggleMenu() {
+    const menu = document.getElementById("menu");
+    menu.style.display = menu.style.display === "block" ? "none" : "block";
+  }
+  
+  window.onclick = function(e) {
+    if (!e.target.matches('.menu-button')) {
+      const menu = document.getElementById("menu");
+      if (menu) menu.style.display = "none";
+    }
+  };
