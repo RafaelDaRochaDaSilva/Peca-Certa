73f20d0a79babed1e1288e94d6cfb3c824f5a81b
juliocesar1404 <julio06.rosa@catolicasc.edu.br>
2025-05-07 14:47:03 -0300

    atualizacao do js

--- js/script.js
+++ js/script.js
@@ -10,20 +10,5 @@ function toggleMenu() {
     }
   };
 
-  const searchBar = document.getElementById("searchBar");
-
-  searchBar.addEventListener("input", function () {
-    const searchValue = searchBar.value.toLowerCase();
-    const carros = document.querySelectorAll("a[data-nome]");
-
-    carros.forEach((carro) => {
-      const nome = carro.getAttribute("data-nome").toLowerCase();
-      if (nome.includes(searchValue)) {
-        carro.style.display = "inline-block";
-      } else {
-        carro.style.display = "none";
-      }
-    });
-  });
 
   
